479a194545e7c458de66553dd4b6f097d37c739e
alireza <alirezadl.73@gmail.com>
2018-12-15 13:00:09 +0330

    added log to file

--- PizzaOffer/Program.cs
+++ PizzaOffer/Program.cs
@@ -19,6 +19,14 @@ namespace PizzaOffer
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .ConfigureLogging((hostingContext, logging) => 
+                {
+                    logging.ClearProviders();
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                    logging.AddDebug();
+                    logging.AddFile(hostingContext.Configuration.GetSection("Logging"));
+                });
     }
 }
