c463c4b70665a700e4422f744960068aa22e9e23
alireza <alirezadl.73@gmail.com>
2019-01-23 17:46:50 +0330

    Added OrderDetail Entity Configuration

--- PizzaOffer.DomainClasses/OrderDetail.cs
+++ PizzaOffer.DomainClasses/OrderDetail.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,10 +8,20 @@ namespace PizzaOffer.DomainClasses
 {
     public class OrderDetail : BaseEntity
     {
+        public int Count { get; set; }
         public int OrderId { get; set; }
         public int FoodId { get; set; }
 
         public virtual Order Order { get; set; }
         public virtual Food Food { get; set; }
+    }
+
+    public class OrderDetailConfiguration : IEntityTypeConfiguration<OrderDetail>
+    {
+        public void Configure(EntityTypeBuilder<OrderDetail> builder)
+        {
+            builder.HasOne(q => q.Order).WithMany(q => q.OrderDetails).HasForeignKey(q => q.OrderId);
+            builder.HasOne(q => q.Food).WithMany(q => q.OrderDetails).HasForeignKey(q => q.FoodId);
+        }
     }
 }
