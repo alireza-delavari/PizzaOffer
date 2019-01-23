919ab2e9087fad90ddf0697140cc45fe6e263353
alireza <alirezadl.73@gmail.com>
2019-01-23 17:36:44 +0330

    Added User Entity Configuration

--- PizzaOffer.DomainClasses/User.cs
+++ PizzaOffer.DomainClasses/User.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,6 +10,7 @@ namespace PizzaOffer.DomainClasses
     {
         public User()
         {
+            //Todo: check if HashSet is good(null object patterns)(dissabling lazy loading)
             Orders = new HashSet<Order>();
         }
 
@@ -19,7 +22,31 @@ namespace PizzaOffer.DomainClasses
         public bool IsPhoneConfirmed { get; set; }
         public bool IsEmailConfirmed { get; set; }
         public bool IsUserActive { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public DateTimeOffset? UpdatedDate { get; set; }
+        public DateTimeOffset? LastLoginDate { get; set; }
 
         public virtual ICollection<Order> Orders { get; set; }
+    }
+
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.Property(q => q.UserName).HasMaxLength(50).IsRequired();
+            builder.HasIndex(q => q.UserName).IsUnique();
+
+            builder.Property(q => q.Password).HasMaxLength(100).IsRequired();
+            builder.Property(q => q.DisplayName).HasMaxLength(50);
+
+            builder.Property(q => q.PhoneNumber).HasMaxLength(20);
+            builder.HasIndex(q => q.PhoneNumber).IsUnique();
+
+            builder.Property(q => q.Email).HasMaxLength(254);
+            builder.HasIndex(q => q.Email).IsUnique();
+
+            builder.Property(q => q.CreatedDate).ValueGeneratedOnAdd().HasDefaultValueSql("SYSDATETIMEOFFSET()");
+            builder.Property(q => q.UpdatedDate).ValueGeneratedOnAddOrUpdate().HasDefaultValueSql("SYSDATETIMEOFFSET()");
+        }
     }
 }
