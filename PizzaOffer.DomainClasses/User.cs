9f7ad854253426e27009fb5f6b20357c419f8cd4
alireza <alirezadl.73@gmail.com>
2019-01-26 01:49:54 +0330

    Changed User Entity

--- PizzaOffer.DomainClasses/User.cs
+++ PizzaOffer.DomainClasses/User.cs
@@ -12,6 +12,8 @@ namespace PizzaOffer.DomainClasses
         {
             //Todo: check if HashSet is good(null object patterns)(dissabling lazy loading)
             Orders = new HashSet<Order>();
+            UserRoles = new HashSet<UserRole>();
+            UserTokens = new HashSet<UserToken>();
         }
 
         public string UserName { get; set; }
@@ -19,13 +21,27 @@ namespace PizzaOffer.DomainClasses
         public string DisplayName { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
+        public string AvatarImage { get; set; }
+        public int AccessFailedCount { get; set; }
         public bool IsPhoneConfirmed { get; set; }
         public bool IsEmailConfirmed { get; set; }
         public bool IsUserActive { get; set; }
+        public bool TwoFactorEnabled { get; set; }
+        public bool LockoutEnabled { get; set; }
         public DateTimeOffset CreatedDate { get; set; }
         public DateTimeOffset? UpdatedDate { get; set; }
         public DateTimeOffset? LastVisitDate { get; set; }
+        public DateTimeOffset? LockoutEnd { get; set; }
+        
+        /// <summary>
+        /// every time the user changes his Password,
+        /// or an admin changes his Roles or stat/IsActive,
+        /// create a new `SerialNumber` GUID and store it in the DB.
+        /// </summary>
+        public string SerialNumber { get; set; }
 
+        public virtual ICollection<UserRole> UserRoles { get; set; }
+        public virtual ICollection<UserToken> UserTokens { get; set; }
         public virtual ICollection<Order> Orders { get; set; }
     }
 
@@ -38,6 +54,8 @@ namespace PizzaOffer.DomainClasses
 
             builder.Property(q => q.Password).HasMaxLength(100).IsRequired();
             builder.Property(q => q.DisplayName).HasMaxLength(50);
+            builder.Property(e => e.SerialNumber).HasMaxLength(450);
+            builder.Property(e => e.AvatarImage).HasMaxLength(2048);
 
             builder.Property(q => q.PhoneNumber).HasMaxLength(20);
             builder.HasIndex(q => q.PhoneNumber).IsUnique();
