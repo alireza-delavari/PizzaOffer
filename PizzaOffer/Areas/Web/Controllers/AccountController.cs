8932b87bd0a42ea6ecbc8e8b6be95c275864dd3c
alireza <alirezadl.73@gmail.com>
2019-02-07 23:09:33 +0330

    Fixed AccountController and Added Login and Register actions

--- PizzaOffer/Areas/Web/Controllers/AccountController.cs
+++ PizzaOffer/Areas/Web/Controllers/AccountController.cs
@@ -1,14 +1,183 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using PizzaOffer.Areas.Web.Models;
+using PizzaOffer.Common;
+using PizzaOffer.DomainClasses;
+using PizzaOffer.Services;
 
 namespace PizzaOffer.Areas.Web.Controllers
 {
+    [Area("Web")]
+    [Route("[area]/[controller]")]
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        private readonly IUsersService _usersService;
+        private readonly IRolesService _rolesService;
+        private readonly IConfiguration _configuration;
+
+        public AccountController(
+            IUsersService usersService,
+            IRolesService rolesService,
+            IConfiguration configuration)
+        {
+            _usersService = usersService;
+            _usersService.CheckArgumentIsNull(nameof(usersService));
+
+            _rolesService = rolesService;
+            _rolesService.CheckArgumentIsNull(nameof(rolesService));
+
+            _configuration = configuration;
+            _configuration.CheckArgumentIsNull(nameof(configuration));
+        }
+
+        [AllowAnonymous]
+        [HttpGet("[action]")]
+        public IActionResult Index(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View(nameof(Login));
+        }
+
+        [AllowAnonymous]
+        [HttpGet("[action]")]
+        public IActionResult Login(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost("[action]")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel loginModel, string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            if (ModelState.IsValid)
+            {
+                var user = await _usersService.FindUserAsync(loginModel.Username, loginModel.Password);
+                if (user == null || !user.IsUserActive)
+                {
+                    await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+                    if (user == null)
+                        ModelState.AddModelError("", "User not found with this username or password");
+                    else if (!user.IsUserActive)
+                        ModelState.AddModelError("", "This user is not Active");
+                    return View(loginModel);
+                }
+
+                await DoLogin(user, loginModel.RememberMe);
+
+                if (Url.IsLocalUrl(returnUrl))
+                {
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                    return RedirectToAction(nameof(HomeController.Index), "Home");
+                }
+            }
+            return View(loginModel);
+        }
+
+        [AllowAnonymous]
+        [HttpGet("[action]")]
+        public IActionResult Register(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost("[action]")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel registerModel, string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+
+            if (ModelState.IsValid)
+            {
+                var newUser = new User
+                {
+                    Username = registerModel.Username,
+                    DisplayName = registerModel.DisplayName,
+                    IsUserActive = true,
+                    LastVisitDate = null,
+                };
+                var result = await _usersService.CreateUserAsync(newUser, registerModel.Password);
+                if (result.Succeeded)
+                {
+                    var user = await _usersService.FindUserAsync(registerModel.Username, registerModel.Password);
+                    if (user != null && user.IsUserActive)
+                    {
+                        await DoLogin(user, false);
+                    }
+
+                    if (Url.IsLocalUrl(returnUrl))
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction(nameof(HomeController.Index), "Home");
+                    }
+                }
+                ModelState.AddModelError(string.Empty, result.Error);
+            }
+            return View(registerModel);
+        }
+
+        private async Task DoLogin(User user, bool rememberMe)
+        {
+            var loginCookieExpirationDays = _configuration.GetValue<int>("LoginCookieExpirationDays", defaultValue: 30);
+            var cookieClaims = await CreateCookieClaimsAsync(user);
+
+            await HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                cookieClaims,
+                new AuthenticationProperties
+                {
+                    IsPersistent = rememberMe, // "Remember Me"
+                    IssuedUtc = DateTimeOffset.UtcNow,
+                    ExpiresUtc = DateTimeOffset.UtcNow.AddDays(loginCookieExpirationDays)
+                });
+
+            await _usersService.UpdateUserLastActivityDateAsync(user.Id);
+        }
+
+        private async Task<ClaimsPrincipal> CreateCookieClaimsAsync(User user)
+        {
+            var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+            identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()));
+            identity.AddClaim(new Claim(ClaimTypes.Name, user.Username));
+            identity.AddClaim(new Claim("DisplayName", user.DisplayName));
+
+            // to invalidate the cookie
+            identity.AddClaim(new Claim(ClaimTypes.SerialNumber, user.SerialNumber));
+
+            // custom data
+            identity.AddClaim(new Claim(ClaimTypes.UserData, user.Id.ToString()));
+
+            // add roles
+            var roles = await _rolesService.FindUserRolesAsync(user.Id).ConfigureAwait(false);
+            foreach (var role in roles)
+            {
+                identity.AddClaim(new Claim(ClaimTypes.Role, role.Name));
+            }
+
+            return new ClaimsPrincipal(identity);
+        }
+
+        [HttpGet("[action]")]
+        [AllowAnonymous]
+        public IActionResult AccessDenied()
         {
             return View();
         }
